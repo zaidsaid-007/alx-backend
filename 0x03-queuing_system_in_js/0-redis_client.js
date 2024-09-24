7e57d17a1f679b36fe69034da364e8584bf609ba
zaidsaid-007 <zaidsaid007@gmail.com>
2024-09-24 14:20:38 +0300

    connecting redis

---
+++ 0x03-queuing_system_in_js/0-redis_client.js
@@ -0,0 +1,11 @@
+//connecting redis server to local machine
+import { createClient } from 'redis';
+
+const client = createClient();
+ client.on ('connect', () => {
+    console.log ('Redis client connected to the server');
+ });
+
+ client.on('error',(error) =>{
+    console.log('Redis client not connected to the server', error.message);
+    });
